0f52b1ecb3a8bf88f10eadb639bca773b45a06c0
OrgalorgTheGorge <145335419+Qwncyy@users.noreply.github.com>
2024-04-15 12:20:00 +0100

    Venv

---
+++ Main/PrototypeRZA.py
@@ -0,0 +1,42 @@
+import tkinter as tk
+from tkinter import *
+from customtkinter import *
+import customtkinter
+
+
+class HomePage():
+
+    def Startup():
+        home = CTk()
+
+        customtkinter.set_default_color_theme("green")
+        customtkinter.set_appearance_mode('dark')
+        home.title('Riget Zoo Adventures')
+        home.geometry('600x350')
+    
+
+        home.mainloop()
+    Startup()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def main():
+
+    
+
+    HomePage()
+main()
+
+
